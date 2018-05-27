c9895323e1f81901e0fec9b72532efe52fdd1892
QINGDONG <935821414@qq.com>
2018-05-27 19:58:56 +0800

    ues vue-router

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Test from '@/view/test'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/test/:testId/name/:testName',
+      name: 'HelloWorld',
+      component: Test
+    }
+  ]
+})
---
+++ src/view/test.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="test">
+this is id :{{$route.params.testId}}
+<br/>
+this is name:{{$route.params.testName}}
+</div>
+</template>
+<script>
+export default{}
+</script>
+<style scoped>
+</style>

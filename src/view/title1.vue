6d334ad7b7b564e859009968cb342c031b66564c
QINGDONG <935821414@qq.com>
2018-05-27 22:48:23 +0800

    vue router qiantao

---
+++ src/view/title1.vue
@@ -0,0 +1,10 @@
+<template>
+<div class="title1">
+这里是title1
+</div>
+</template>
+<script>
+export default{
+}
+</script>
+<style scoped></style>
---
+++ src/view/title2.vue
@@ -0,0 +1,10 @@
+<template>
+<div class="title2">
+这里是title2
+</div>
+</template>
+<script>
+export default{
+}
+</script>
+<style scoped></style>

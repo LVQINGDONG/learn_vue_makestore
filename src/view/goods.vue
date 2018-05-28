d2fb87fcf9d00fbad1f9726b27316e1914aa0e5a
QINGDONG <935821414@qq.com>
2018-05-28 12:56:49 +0800

    add gotoGoods

---
+++ src/view/goods.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class='goods'>
+        This is goods
+    </div>
+</template>
+<script>
+export default{
+    
+}
+</script>
+<style scoped></style>
--- src/view/test.vue
+++ src/view/test.vue
@@ -5,10 +5,21 @@ this is id :{{$route.params.testId}}
 <router-link to="/test/title1">标题1</router-link>
 <router-link to="/test/title2">标题2</router-link>
 <router-view></router-view>
+<div @click="gotoGoods">点击跳转到商品详情页面</div>
 </div>
 </template>
 <script>
 export default{
+    data(){
+        return{
+            msg:'hello vue'
+        }
+    },
+    methods:{
+        gotoGoods(){
+            this.$router.push('/goods');
+        }
+    }
 }
 </script>
 <style scoped>
